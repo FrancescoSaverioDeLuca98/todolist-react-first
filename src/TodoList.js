ca03bbd01c72fc1c8c4c63c85a24706b5bbc5a62
Francesco Saverio Repo <f.saverio.deluca@gmail.com>
2022-07-05 09:38:48 +0200

    init proj setup

---
+++ src/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class TodoList extends Component {
+  render() {
+    return (
+    <>    
+    <div>Dioporco</div>
+    <input type="text" />
+    <button>Add</button>
+    <button>Clear</button>
+    <div>number to do left</div>
+    </>
+
+    )
+  }
+}
